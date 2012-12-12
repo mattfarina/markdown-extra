d13c6caa29263babf30e2893a6a4dde8c8c572b4
Matt Farina <matt@mattfarina.com>
2012-12-12 09:16:01 -0500

    Updating to use new download location (hp cloud object storage) rather than github who is no longer hosting downloads.

--- config/000-init.php
+++ config/000-init.php
@@ -40,7 +40,7 @@ $registry->route('self-update', 'Update the application if there is a newer vers
     ->using('version1')->from('cxt:version1')
     ->using('version2')->from('cxt:version2')
   ->does('\Fortissimo\CLI\Update\Update')
-    ->using('file', 'https://github.com/downloads/mattfarina/markdown-extra/markdown-extra')
+    ->using('file', 'http://download.mattfarina.com/markdown-extra')
     ->using('doUpdate')->from('cxt:versionDiff')
   ;
 
